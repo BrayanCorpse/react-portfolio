d2bb9994f7c2c11305001ccc34c762f563701f3b
bryancorpse <al221610173@gmail.com>
2020-07-28 15:47:03 -0500

    new portfolio

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import "./css/estilos.css";
-import './iconos/font-awesome-4.7.0/css/font-awesome.min.css';
-// import Reportes from './pages/Reportes';
-import Hosting from './componentes/Hosting';
-
-
-
-
-
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
- 
-  <div>
-  <Hosting/> 
-  </div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
